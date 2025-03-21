4eab1fc9dc93d1f01527efed022d8017a80a49ec
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-03-20 19:36:28 -0500

    Add BookService service

---
+++ src/main/java/me/santiagocuervo/service/BookService.java
@@ -0,0 +1,41 @@
+package me.santiagocuervo.service;
+
+import java.util.List;
+
+import me.santiagocuervo.exception.NotFoundException;
+import me.santiagocuervo.model.Book;
+
+public class BookService {
+
+    private List<Book> books;
+
+    public void addBook(String isbn, String title, String author) {
+        books.add(new Book(isbn, title, author));
+    }
+
+    public List<Book> getBooks() {
+        return books;
+    }
+
+    public Book getBookByIsbn(String isbn) throws NotFoundException {
+        for (var book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                return book;
+            }
+        }
+
+        throw new NotFoundException(String.format("El libro con ISBN '%s', no ha sido encontrado.", isbn));
+    }
+
+    public void deleteBook(String isbn) throws NotFoundException {
+        for (var book : books) {
+            if (book.getIsbn().equals(isbn)) {
+                books.remove(book);
+                return;
+            }
+        }
+
+        throw new NotFoundException(String.format("El libro con ISBN '%s', no ha podido ser eliminado.", isbn));
+    }
+
+}
