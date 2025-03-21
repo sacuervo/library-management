bf4a7eb29f33b36489512793d350469c248cd5b2
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-03-21 11:04:35 -0500

    Add name and email setters to User

--- src/main/java/me/santiagocuervo/model/User.java
+++ src/main/java/me/santiagocuervo/model/User.java
@@ -27,8 +27,16 @@ public class User {
         return name;
     }
 
+    public void setName(String name) {
+        this.name = name;
+    }
+
     public String getEmail() {
         return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
     }
 
     public LocalDate getRegisterDate() {
