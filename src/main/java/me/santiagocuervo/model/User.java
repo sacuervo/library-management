e4466c609986595a274f89cf76a40cd158a5f843
Santiago Cuervo <cuervo.santiago@gmail.com>
2025-03-20 19:09:46 -0500

    Add User model

---
+++ src/main/java/me/santiagocuervo/model/User.java
@@ -0,0 +1,43 @@
+package me.santiagocuervo.model;
+
+public class User {
+
+}
+package me.santiagocuervo.model;
+
+import java.time.LocalDate;
+
+public class User {
+
+    private String id;
+    private String name;
+    private String email;
+    private LocalDate registerDate;
+
+    public User(String id, String name, String email, LocalDate registerDate) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+    }
+
+    public User(String id, String name, String email) {
+        this(id, name, email, LocalDate.now());
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getRegisterDate() {
+        return registerDate;
+    }
+
+}
